bcb20a93004e8fba6d5005c4556279ad29114e4a
Ashutosh Kumar Giri <ashutoshashish1012@gmail.com>
2024-04-27 07:26:11 +0530

    Feature Updates

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,11 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.models.js"
+
+const registerUser = asyncHandler(async (req,res)=>{
+     if([fullName,email,user,password].some((field)=>field?.trim()===""))
+     throw new ApiError(404,"All field are required")
+     }
+})
+
+export {registerUser}
